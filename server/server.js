04d79ff84c9f03532e86a08d7682533205472519
David Diaz <daemdisa@aol.com>
2019-09-11 13:45:52 -0500

    after lunch

--- server/server.js
+++ server/server.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const bodyParser = require( 'body-parser' );
+const pg = require( 'pg' );
+
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'music_library',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+})
+
+pool.on( 'connect', () => {
+    console.log( 'Postgress connected' );
+})
+
+pool.on( 'error', (error) => {
+    console.log( 'Error with Postgress pool', error );
+})
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,8 +26,17 @@ app.use(express.static('server/public'));
 app.use( bodyParser.urlencoded( { extended: true } ) )
 
 
-let musicRouter = require('./routes/music_router');
-app.use('/musicLibrary', musicRouter);
+// let musicRouter = require('./routes/music_router');
+// app.use('/musicLibrary', musicRouter);
+
+app.get('/musicLibrary', (req, res) => {
+    // res.send(musicLibrary);
+    let queryText = `SELECT * FROM "songs";`;
+    pool.query( queryText ).then((result) => {
+        // console.log("resultsssssss", result );
+        res.send( result.rows );
+    })
+});
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
