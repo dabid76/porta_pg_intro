5072c46df593eebf6c4a47f9e93126b8b8dd2990
David Diaz <daemdisa@aol.com>
2019-09-11 14:39:38 -0500

    little break

--- server/routes/music_router.js
+++ server/routes/music_router.js
@@ -1,49 +1,32 @@
 const express = require('express');
 const router = express.Router();
-
-let musicLibrary = [
-    {
-        artist: "Sturgill Simpson",
-        track: "Keep It Between the Lines",
-        rank: 1,
-        published: "4-16-2016"
-    },
-    {
-        artist: "Margo Price",
-        track: "Since You Put Me Down",
-        rank: 2,
-        published: "3-25-2016"
-    },
-    {
-        artist: "Jason Isbell",
-        track: "Alabama Pines",
-        rank: 3,
-        published: "4-12-2011"
-    },
-    {
-        artist: "Midland",
-        track: "Check Cashin' Country",
-        rank: 4,
-        published: "9-22-2017"
-    },
-    {
-        artist: "Nikki Lane",
-        track: "Highway Queen",
-        rank: 5,
-        published: "2-17-2017"
-    }
-]
+const pool = require( '../modules/pool')
 
 router.get('/', (req, res) => {
-    // res.send(musicLibrary);
     let queryText = `SELECT * FROM "songs";`;
-
+    pool.query( queryText ).then((result) => {
+        res.send( result.rows );
+    })
+    .catch(( err ) => {
+        console.log("error making query", err);
+        res.sendStatus(500);
+    })
 });
 
 router.post('/', (req, res) => {
     console.log("HELLO FROM THE POST", req.body);
-    res.sendStatus(200);
-    musicLibrary.push(req.body);
+    const newSong = req.body;
+    const queryText = `INSERT INTO "songs" ("artist", "track", "rank", "published")
+    VALUES ('${newSong.artist}', '${newSong.track}', ${newSong.rank}, '${newSong.published}');`
+
+    pool.query( queryText ).then(( result ) => {
+        console.log( "POST RESULTSSSSS", result);
+        res.sendStatus(201);
+    })
+    .catch((err) => {
+        console.log(`error making query ${queryText}`, err);
+        res.sendStatus(500);
+    })
 })
 
 module.exports = router;
